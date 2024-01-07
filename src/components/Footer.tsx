11640faf2800bb6960d8e345f78c708478ee61bd
kmbui <mkb18@sfu.ca>
2024-01-06 22:09:17 -0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <a href="#about_me" className={styles.back_to_top}>
             <span className={styles.container}>↑ Back to top </span>
         </a>
-            <span className={styles.last_updated}>Last Updated January 2024</span>
+            <span className={styles.last_updated}>Last Updated: January 2024</span>
       </footer>
     );
   }
