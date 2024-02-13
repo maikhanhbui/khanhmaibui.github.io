de8c086d9ed569eeb1a21c53e7d9223c942c0b3f
kmbui <mkb18@sfu.ca>
2024-02-13 13:07:38 -0800

    remove underline

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import styles from "../styles/Footer.module.scss";
 export default function Footer() {
     return (
       <footer className={styles.container}>
-        <a href="#about_me" className={styles.back_to_top}>
-            <span className={styles.container}>↑ Back to top </span>
+        <a href="#about_me" className={styles.container}>
+            <span>↑ Back to top </span>
         </a>
             <span className={styles.last_updated}>Last Updated: February 2024</span>
       </footer>
